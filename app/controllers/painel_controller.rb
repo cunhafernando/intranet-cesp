ef3fb33bf0b74d837c1938d897f34183b883766b
Fernando Cunha <cunhafernandofj@gmail.com>
2022-08-17 14:52:37 -0300

    invertendo formula media leitos ocupados 2

--- app/controllers/painel_controller.rb
+++ app/controllers/painel_controller.rb
@@ -13,7 +13,7 @@ class PainelController < ApplicationController
     @leitos_extras = CensoDiario.where("leito LIKE ?", "%EXTRA%").count(:leito)
     @leitos_extras_ocupados = ((CensoDiario.where.not(boletim:[nil, ""])).and(CensoDiario.where("leito LIKE ?", "%EXTRA%"))).count(:leito)
     @leitos_total = CensoDiario.where.not("leito LIKE ?", "%EXTRA%").count(:leito)
-    @media_leitos_ocupados = (@leitos_ocupados * 100) / @leitos_total # ((@leitos_ocupados.to_f / @leitos_total.to_f) * 100).to_i
+    @media_leitos_ocupados = @leitos_ocupados * 100 # ((@leitos_ocupados.to_f / @leitos_total.to_f) * 100).to_i
     @color_meta_leitos_ocupados = if @media_leitos_ocupados <= 85 
       "success" 
     else 
